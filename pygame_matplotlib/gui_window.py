d112f9cde258836eed3631813bdd9232ca1bb714
lionel42 <43442120+lionel42@users.noreply.github.com>
2021-10-12 02:07:29 +0200

    adding gui window for the plot

---
+++ pygame_matplotlib/gui_window.py
@@ -0,0 +1,42 @@
+"""Contain a window with a plot for pygame_gui."""
+from typing import Union
+
+import pygame
+import pygame_gui
+from pygame_gui.core.interfaces.manager_interface import IUIManagerInterface
+from pygame_gui.core.ui_element import ObjectID
+
+from .backend_pygame import FigureSurface
+import matplotlib
+matplotlib.use('module://pygame_matplotlib.backend_pygame')
+
+class PlotWindow(pygame_gui.elements.ui_window.UIWindow):
+    def __init__(
+        self,
+        rect: pygame.Rect,
+        manager: IUIManagerInterface,
+        figuresurface: FigureSurface,
+        window_display_title: str = "",
+        element_id: Union[str, None] = None,
+        object_id: Union[ObjectID, str, None] = None,
+        resizable: bool = False,
+        visible: int = 1
+    ):
+        self.figuresurf = figuresurface
+        super().__init__(
+            rect, manager,
+            window_display_title=window_display_title,
+            element_id=element_id,
+            object_id=object_id,
+            resizable=resizable,
+            visible=visible
+        )
+
+    def set_dimensions(self, *args, **kwargs):
+        super().set_dimensions(*args, **kwargs)
+        # Update the size of the figure with the new bounding rectangle
+        self.figuresurf.set_bounding_rect(
+            self.get_container().get_rect()
+        )
+        # Update the image of the container
+        self.get_container().set_image(self.figuresurf)
---
+++ test_gui_window.py
@@ -0,0 +1,78 @@
+"""Simple script to test from https://pygame-gui.readthedocs.io/en/latest/quick_start.html"""
+
+from typing import Tuple, Union
+import pygame
+import pygame_gui
+from pygame_gui.core.interfaces.manager_interface import IUIManagerInterface
+from pygame_gui.core.ui_element import ObjectID
+
+from pygame_gui.elements.ui_window import UIWindow
+from pygame_gui.core.utility import basic_blit
+
+
+import matplotlib.pyplot as plt
+
+from pygame_matplotlib.gui_window import PlotWindow
+
+
+pygame.init()
+
+pygame.display.set_caption('Test')
+window_surface = pygame.display.set_mode((800, 600))
+
+background = pygame.Surface((800, 600))
+background.fill(pygame.Color('#000000'))
+
+manager = pygame_gui.UIManager((800, 600))
+
+fig, axes = plt.subplots(1, 1,)
+axes.plot([1,2], [1,2], color='green', label='test')
+fig.canvas.draw()
+
+
+fig2, axes2 = plt.subplots(1, 1,)
+axes2.plot([1,2], [1,2], color='blue', label='test')
+fig2.canvas.draw()
+
+
+plot_window = PlotWindow(
+    rect=pygame.Rect((350, 275), (300, 200)),
+    manager=manager,
+    figuresurface=fig,
+    resizable=True
+)
+
+
+
+
+plot_window2 = PlotWindow(
+    rect=pygame.Rect((350, 275), (300, 200)),
+    manager=manager,
+    figuresurface=fig2,
+    resizable=True
+)
+
+
+clock = pygame.time.Clock()
+
+is_running = True
+
+while is_running:
+    time_delta = clock.tick(60)/1000.0
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+
+
+        manager.process_events(event)
+
+    manager.update(time_delta)
+
+    # Blitts and draw
+    window_surface.blit(background, (0, 0))
+    manager.draw_ui(window_surface)
+
+
+    # print(plot_window2.get_container().get_size())
+
+    pygame.display.update()
