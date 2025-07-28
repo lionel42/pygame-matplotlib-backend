0a1a1c859db19a859b5c4b4e83b01ff4b48e1811
Lionel42 <constantin.lionel@hotmail.com>
2025-07-28 11:37:41 +0200

    cleaned examples

--- examples/show.py
+++ examples/show.py
@@ -1,5 +1,18 @@
+import sys
+import os
+from numpy.ma.core import size
+import pygame
 import numpy as np
+
+import matplotlib
+#matplotlib.use('pygame')
+matplotlib.use('pygame')
+
 import matplotlib.pyplot as plt
+import matplotlib.figure as fg
+
+import matplotlib.image as mpimg
+
 
 def plot_error_bars_ex(ax):
     # example data
@@ -105,3 +118,20 @@ def plot_violin(ax):
     # set style for the axes
     labels = ['A', 'B', 'C', 'D']
     set_axis_style(ax, labels)
+
+fig, axes = plt.subplots(3,2,figsize=(16, 12))
+print(type(fig))
+
+axes[0, 0].plot([1,2], [1,2], color='green', label='test')
+axes[0, 0].plot([1,2], [1,1], color='orange', lw=5, label='test other larger')
+# axes[0, 0].legend()
+axes[0, 1].text(0.5, 0.5, '2', size=50)
+axes[1, 0].set_xlabel('swag')
+axes[1, 0].fill_between([0, 1, 2], [1, 2, 3], [3, 4, 5])
+axes[1, 0].scatter([0, 1, 2], [2, 3, 4], s=50)
+axes[1, 1].imshow(mpimg.imread('images' + os.sep + 'long_dog.jpg'))
+plot_error_bars_ex(axes[2, 1])
+
+plot_violin(axes[2, 0])
+
+plt.show()
